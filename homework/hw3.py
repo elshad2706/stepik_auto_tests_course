8a4557a943783a9420c4480c32c2c9c2d5ab42f5
unknown <elshadmursalov8@gmail.com>
2024-01-08 16:33:32 +0300

    confirm to allert

--- homework/hw3.py
+++ homework/hw3.py
@@ -3,33 +3,22 @@ from selenium.webdriver.common.by import By
 import math
 import time
 
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
 try:
     browser = webdriver.Chrome()
-    link = "http://suninjuly.github.io/get_attribute.html"
+    link = "http://suninjuly.github.io/alert_accept.html"
     browser.get(link)
-
-    sunduk = browser.find_element(By.ID,"treasure")
-    sunduk_atribute = sunduk.get_attribute("valuex")
-    print("значение атрибута", sunduk_atribute)
-    assert sunduk_atribute is not None
-
-    def calc(x):
-        return str(math.log(abs(12*math.sin(int(x)))))
-
-    y = calc(sunduk_atribute)
-
-    input_text = browser.find_element(By.ID,"answer")
-    input_text.send_keys(y)
-    time.sleep(1)
-    robot_checkbox = browser.find_element(By.ID,"robotCheckbox")
-    robot_checkbox.click()
-    time.sleep(1)
-    robots_Rule = browser.find_element(By.ID,"robotsRule")
-    robots_Rule.click()
-    time.sleep(1)
-    submit = browser.find_element(By.CSS_SELECTOR,"button")
-    submit.click()
-    time.sleep(3)
-
+    button = browser.find_element(By.CSS_SELECTOR,'.btn.btn-primary').click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    input = browser.find_element(By.ID,'answer')
+    input.send_keys(y)
+    submit = browser.find_element(By.CSS_SELECTOR,'.btn.btn-primary').click()
 finally:
+    time.sleep(4)
     browser.quit()
