ada78bb8c81c765ef26b7ca77cc398f6c833490b
unknown <elshadmursalov8@gmail.com>
2024-01-08 16:38:47 +0300

    switch window

--- homework/hw4.py
+++ homework/hw4.py
@@ -2,17 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import math
 import time
-
 def calc(x):
     return str(math.log(abs(12*math.sin(int(x)))))
 
 try:
     browser = webdriver.Chrome()
-    link = "http://suninjuly.github.io/alert_accept.html"
+    link = "http://suninjuly.github.io/redirect_accept.html"
     browser.get(link)
-    button = browser.find_element(By.CSS_SELECTOR,'.btn.btn-primary').click()
-    confirm = browser.switch_to.alert
-    confirm.accept()
+    button = browser.find_element(By.CSS_SELECTOR,'[type="submit"]').click()
+    time.sleep(1)
+    new_tab_handle = browser.window_handles[1]
+    browser.switch_to.window(new_tab_handle)
+    time.sleep(1)
     x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
     x = x_element.text
     y = calc(x)
