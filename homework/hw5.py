22eb0289ff865c6130edf6fea227dbb8ff346819
unknown <elshadmursalov8@gmail.com>
2024-01-08 16:39:14 +0300

    implicitly_wait

--- homework/hw5.py
+++ homework/hw5.py
@@ -1,25 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import math
-import time
-def calc(x):
-    return str(math.log(abs(12*math.sin(int(x)))))
 
-try:
-    browser = webdriver.Chrome()
-    link = "http://suninjuly.github.io/redirect_accept.html"
-    browser.get(link)
-    button = browser.find_element(By.CSS_SELECTOR,'[type="submit"]').click()
-    time.sleep(1)
-    new_tab_handle = browser.window_handles[1]
-    browser.switch_to.window(new_tab_handle)
-    time.sleep(1)
-    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
-    x = x_element.text
-    y = calc(x)
-    input = browser.find_element(By.ID,'answer')
-    input.send_keys(y)
-    submit = browser.find_element(By.CSS_SELECTOR,'.btn.btn-primary').click()
-finally:
-    time.sleep(4)
-    browser.quit()
+browser = webdriver.Chrome()
+# говорим WebDriver искать каждый элемент в течение 5 секунд
+browser.implicitly_wait(5)
+
+browser.get("http://suninjuly.github.io/cats.html?")
+
+button = browser.find_element(By.ID, "button")
+button.click()
+message = browser.find_element(By.ID, "verify_message")
+
+assert "successful" in message.text
