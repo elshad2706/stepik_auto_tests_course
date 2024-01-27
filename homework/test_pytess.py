6ea3d696d5db49ff1d6bd324e13a96e6c4681293
unknown <elshadmursalov8@gmail.com>
2024-01-27 19:21:00 +0300

    new

--- homework/test_pytess.py
+++ homework/test_pytess.py
@@ -1,69 +1,54 @@
-import unittest
+import pytest
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
 from selenium.webdriver.common.by import By
 
 
-class test_class_name(unittest.TestCase):
-    def setUp(self):
-        # This method will be called before each test method
-        self.browser = webdriver.Chrome()  # You can use any other webdriver here
-
-    def tearDown(self):
-        # This method will be called after each test method
-        self.browser.quit()
-    def test_registration_page1(self):
-        self.browser.get("http://suninjuly.github.io/registration1.html")
+def test_registration_page1():
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration1.html")
 
         # Ваш код для заполнения формы на странице
-        button1 = self.browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your first name"]')
+        button1 = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your first name"]')
         button1.send_keys("Name")
-        button2 = self.browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your last name"]')
+        button2 = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your last name"]')
         button2.send_keys("Surname")
-        button3 = self.browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your email"]')
+        button3 = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your email"]')
         button3.send_keys("test@mail.ru")
-        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
         time.sleep(1)
 
         # находим элемент, содержащий текст
-        welcome_text_elt = self.browser.find_element(By.TAG_NAME, "h1")
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
 
         # Проверка, что после заполнения формы нет ошибок
-        self.assertEqual(welcome_text,'Congratulations! You have successfully registered!','correct')
+        assert(welcome_text,'Congratulations! You have successfully registered!','correct')
+        browser.quit()
 
-    def test_registration_page2(self):
-        self.browser.get("http://suninjuly.github.io/registration2.html")
-
+def test_registration_page2():
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration2.html")
         # Ваш код для заполнения формы на странице
-
-        button1 = self.browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your first name"]')
+        button1 = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your first name"]')
         button1.send_keys("Name")
-        button2 = self.browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your last name"]')
+        button2 = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your last name"]')
         button2.send_keys("Surname")
-        button3 = self.browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your email"]')
+        button3 = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your email"]')
         button3.send_keys("test@mail.ru")
-
-        # Отправляем заполненную форму
-        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
-
-        # Проверяем, что смогли зарегистрироваться
-        # ждем загрузки страницы
         time.sleep(1)
-
         # находим элемент, содержащий текст
-        welcome_text_elt = self.browser.find_element(By.TAG_NAME, "h1")
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
-
         # Проверка, что после заполнения формы есть ошибка (NoSuchElementException)
-        self.assertEqual(welcome_text, 'Congratulations! You have successfully registered!', 'correct')
-
+        assert(welcome_text, 'Congratulations! You have successfully registered!', 'correct')
+        browser.quit()
 
 if __name__ == "__main__":
-    browser = webdriver.Chrome()
-    unittest.main()
+    pytest.main()
